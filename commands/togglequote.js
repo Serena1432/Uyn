d344ec260c260b4303ae57b3c94c3b12202299e7
Nico Levianth <nambmlc132@gmail.com>
2021-02-16 14:27:03 +0700

    Fixed a typo in togglequote.js

--- commands/togglequote.js
+++ commands/togglequote.js
@@ -16,27 +16,22 @@ module.exports.run = async (client, message, args) => {
     if (args[0] == "on") enabled = 1;
     else if (args[0] != "on" && args[0] != "off") return message.reply("Invalid value!\nYou need to type `on` or `off`!");
         request(process.env.php_server_url + 'ToggleQuote.php?token=' + process.env.php_server_token + '&type=set&server=' + message.author.id + "&enabled=" + enabled, function(err, response, body) {
-            if (!response || body.includes('Connection failed')) {
+            if (!response || response.statusCode != 200 || body.includes('Connection failed')) {
+                console.error(err);
+                console.error(body);
                 message.channel.send("Cannot connect to the BOT server! Please try again!");
             } else {
-                if (body && body.includes('Success')) {
-                    console.log(body);
-                    request(process.env.php_server_url + '/ToggleQuote.php?token=' + process.env.php_server_token + '&type=get', function(error, response, body) {
-                        if (response && !body.includes("Connection failed")) {
-                            client.toggleQuote = JSON.parse(body);
-                            console.log("👌 Ping-responsing toggle mode successfully updated");
-                            if (enabled) message.reply("Successfully enabled sending ping-responsing message.");
-                            else message.reply("Successfully disabled sending ping-responsing message.");
-                        } else {
-                            client.users.cache.get(client.config.ownerId).send("Ping-responsing toggle mode failed to update.");
-                            message.reply("Changing failed! Please try again later!");
-                        }
-                    });
-
-                } else {
-                    console.log(body);
-                    message.reply("Changing failed! Please try again later!");
-                }
+                request(process.env.php_server_url + '/ToggleQuote.php?token=' + process.env.php_server_token + '&type=get', function(error, response, body) {
+                    if (response && !body.includes("Connection failed")) {
+                        client.toggleQuote = JSON.parse(body);
+                        console.log("👌 Ping-responsing toggle mode successfully updated");
+                        if (enabled) message.reply("Successfully enabled sending ping-responsing message.");
+                        else message.reply("Successfully disabled sending ping-responsing message.");
+                    } else {
+                        client.users.cache.get(client.config.ownerId).send("Ping-responsing toggle mode failed to update.");
+                        message.reply("Changing failed! Please try again later!");
+                    }
+                });
             }
         });
 }
