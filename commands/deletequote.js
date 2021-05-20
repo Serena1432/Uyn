2713b9b923ec43438a86d1c5c45899f3aad1c401
LilShieru <nambmlc132@gmail.com>
2021-05-20 20:47:40 +0700

    Fixed a serious error in the deletequote command

--- commands/deletequote.js
+++ commands/deletequote.js
@@ -12,14 +12,14 @@ function random(min, max) {
 
 module.exports.run = async (client, message, args) => {
     if (message.channel.type == "text") return message.reply("This command now can only be used in a Direct Messages channel!"); 
-    request(process.env.php_server_url + 'DeleteQuote.php?token=' + process.env.php_server_token + '&id=' + message.author.id, function(err, response, body) {
+    request(process.env.php_server_url + '/DeleteQuote.php?token=' + process.env.php_server_token + '&id=' + message.author.id, function(err, response, body) {
         if (!response || response.statusCode != 200 || body.includes('Connection failed')) {
             console.log(body);
             message.channel.send("Cannot connect to the BOT server! Please try again!");
         } else {
             if (body && body.includes('Success')) {
                 console.log(body);
-                request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
+                request(process.env.php_server_url + '/GetAllQuotes.php' + message.author.id, function(error, response, body) {
                     console.error('error:', error); // Print the error if one occurred
                     console.log(response.statusCode);
                     if (response && response.statusCode == 200) {
