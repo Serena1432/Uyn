d1c519c1214581df7d1523f6b04f8e36527a059a
LilShieru <nambmlc132@gmail.com>
2021-06-13 20:51:25 +0700

    Fixed the CPU and RAM Usage part since the usage module didn't work

--- commands/stats.js
+++ commands/stats.js
@@ -9,10 +9,12 @@ module.exports.run = async (client, message, args) => {
     message.delete({
         timeout: 10000
     });
-    message.channel.send("", {embed: {
-        color: Math.floor(Math.random() * 16777214) + 1,
-        description: "Connecting to the BOT server, please wait..."
-    }}).then(msg => {
+    message.channel.send("", {
+        embed: {
+            color: Math.floor(Math.random() * 16777214) + 1,
+            description: "Connecting to the BOT server, please wait..."
+        }
+    }).then(msg => {
         let totalSeconds = (client.uptime / 1000);
         let days = Math.floor(totalSeconds / 86400);
         let hours = Math.floor(totalSeconds / 3600);
@@ -21,40 +23,16 @@ module.exports.run = async (client, message, args) => {
         let seconds = totalSeconds % 60;
         var svStatus = "Unknown";
         var quoteInfo = "";
-        try {
-            const usage = require("usage");
-            usage.lookup(process.pid, function(err, result) {
-                if (err) {
-                    console.error(err);
-                    return message.reply("An error has occurred.");
-                }
-                request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
-                    if (response && response.statusCode == 200) {
-                        if (!body.includes("Connection failed")) {
-                            svStatus = "Operational";
-                        } else {
-                            svStatus = "Cannot connect to the SQL server";
-                        }
-                    } else {
-                        svStatus = "Cannot connect to the PHP server!";
-                    }
-                    var dated = new Date();
-                    const mess = {
-                        color: Math.floor(Math.random() * 16777214) + 1,
-                        title: 'BOT\'s current status',
-                        description: "⏰ **BOT's restart time**: " + client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes() + "\n⬆️ **Active time:** " + days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs\n🙂 **Server response:** " + (dated.getTime() - message.createdTimestamp) + " ms\n🖥️ **Server status:** " + svStatus + "\n" + quoteInfo + "🚩 **Joined servers:** " + client.guilds.cache.size + "\n👪 **Number of users:** " + client.users.cache.size + "\n#️⃣ **Channels:** " + client.channels.cache.size + "\n💬 **Number of messages sent (from BOT's last restart):** " + client.sentMessages + "\n🤖 **Number of commands executed (from BOT's last restart):** " + client.botMessages + "\n💭 **Total RAM Usage:** " + (result.memory / 1048576).toFixed(2) + " MB\n🤖 **Total CPU Usage:** " + result.cpu.toFixed(2) + "%",
-                        footer: {
-                            text: client.devUsername
-                        },
-                    };
-                    msg.edit({
-                        embed: mess
-                    });
-                });
-            });
-        }
-        catch (err) {
-            console.log("Cannot find the usage module. Skipping...");
+        const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
+        const memoryData = process.memoryUsage();
+        const memoryUsage = {
+            rss: `${formatMemoryUsage(memoryData.rss)}`,
+            heapTotal: `${formatMemoryUsage(memoryData.heapTotal)}`,
+            heapUsed: `${formatMemoryUsage(memoryData.heapUsed)}`,
+            external: `${formatMemoryUsage(memoryData.external)}`,
+        };
+        var os = require('os-utils');
+        os.cpuUsage(cpu => {
             request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
                 if (response && response.statusCode == 200) {
                     if (!body.includes("Connection failed")) {
@@ -69,7 +47,7 @@ module.exports.run = async (client, message, args) => {
                 const mess = {
                     color: Math.floor(Math.random() * 16777214) + 1,
                     title: 'BOT\'s current status',
-                    description: "⏰ **BOT's restart time**: " + client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes() + "\n⬆️ **Active time:** " + days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs\n🙂 **Server response:** " + (dated.getTime() - message.createdTimestamp) + " ms\n🖥️ **Server status:** " + svStatus + "\n" + quoteInfo + "🚩 **Joined servers:** " + client.guilds.cache.size + "\n👪 **Number of users:** " + client.users.cache.size + "\n#️⃣ **Channels:** " + client.channels.cache.size + "\n💬 **Number of messages sent (from BOT's last restart):** " + client.sentMessages + "\n🤖 **Number of commands executed (from BOT's last restart):** " + client.botMessages,
+                    description: "⏰ **BOT's restart time**: " + client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes() + "\n⬆️ **Active time:** " + days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs\n🙂 **Server response:** " + (dated.getTime() - message.createdTimestamp) + " ms\n🖥️ **Server status:** " + svStatus + "\n" + quoteInfo + "🚩 **Joined servers:** " + client.guilds.cache.size + "\n👪 **Number of users:** " + client.users.cache.size + "\n#️⃣ **Channels:** " + client.channels.cache.size + "\n💬 **Number of messages sent (from BOT's last restart):** " + client.sentMessages + "\n🤖 **Number of commands executed (from BOT's last restart):** " + client.botMessages + "\n💭 **BOT's Memory Usage:**\n- **Resident Set Size:** " + memoryUsage.rss + "\n- **Heap Total Size:** " + memoryUsage.heapTotal + "\n- **Heap Used Size:** " + memoryUsage.heapUsed + "\n- **V8 External Memory:** " + memoryUsage.external + "\n🤖 **CPU Usage:** " + cpu.toFixed(2) + "%",
                     footer: {
                         text: client.devUsername
                     },
@@ -78,7 +56,7 @@ module.exports.run = async (client, message, args) => {
                     embed: mess
                 });
             });
-        }
+        });
     });
 }
 
