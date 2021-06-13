560f9f66e35e3c5c94c5dac6362421f785dc4cba
LilShieru <nambmlc132@gmail.com>
2021-06-13 20:29:08 +0700

    Added the RAM and CPU usage in the stats command

--- commands/stats.js
+++ commands/stats.js
@@ -21,30 +21,64 @@ module.exports.run = async (client, message, args) => {
         let seconds = totalSeconds % 60;
         var svStatus = "Unknown";
         var quoteInfo = "";
-        request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
-            console.log(response.statusCode);
-            if (response && response.statusCode == 200) {
-                if (!body.includes("Connection failed")) {
-                    svStatus = "Operational";
-                } else {
-                    svStatus = "Cannot connect to the SQL server";
+        try {
+            const usage = require("usage");
+            usage.lookup(process.pid, function(err, result) {
+                if (err) {
+                    console.error(err);
+                    return message.reply("An error has occurred.");
                 }
-            } else {
-                svStatus = "Cannot connect to the PHP server!";
-            }
-            var dated = new Date();
-            const mess = {
-                color: Math.floor(Math.random() * 16777214) + 1,
-                title: 'BOT\'s current status',
-                description: "⏰ **BOT's restart time**: " + client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes() + "\n⬆️ **Active time:** " + days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs\n🙂 **Server response:** " + (dated.getTime() - message.createdTimestamp) + " ms\n🖥️ **Server status:** " + svStatus + "\n" + quoteInfo + "🚩 **Joined servers:** " + client.guilds.cache.size + "\n👪 **Number of users:** " + client.users.cache.size + "\n#️⃣ **Channels:** " + client.channels.cache.size + "\n💬 **Number of messages sent (from BOT's last restart):** " + client.sentMessages + "\n🤖 **Number of commands executed (from BOT's last restart):** " + client.botMessages,
-                footer: {
-                    text: client.devUsername
-                },
-            };
-            msg.edit({
-                embed: mess
+                request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
+                    if (response && response.statusCode == 200) {
+                        if (!body.includes("Connection failed")) {
+                            svStatus = "Operational";
+                        } else {
+                            svStatus = "Cannot connect to the SQL server";
+                        }
+                    } else {
+                        svStatus = "Cannot connect to the PHP server!";
+                    }
+                    var dated = new Date();
+                    const mess = {
+                        color: Math.floor(Math.random() * 16777214) + 1,
+                        title: 'BOT\'s current status',
+                        description: "⏰ **BOT's restart time**: " + client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes() + "\n⬆️ **Active time:** " + days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs\n🙂 **Server response:** " + (dated.getTime() - message.createdTimestamp) + " ms\n🖥️ **Server status:** " + svStatus + "\n" + quoteInfo + "🚩 **Joined servers:** " + client.guilds.cache.size + "\n👪 **Number of users:** " + client.users.cache.size + "\n#️⃣ **Channels:** " + client.channels.cache.size + "\n💬 **Number of messages sent (from BOT's last restart):** " + client.sentMessages + "\n🤖 **Number of commands executed (from BOT's last restart):** " + client.botMessages + "\n💭 **Total RAM Usage:** " + (result.memory / 1048576).toFixed(2) + " MB\n🤖 **Total CPU Usage:** " + result.cpu.toFixed(2) + "%",
+                        footer: {
+                            text: client.devUsername
+                        },
+                    };
+                    msg.edit({
+                        embed: mess
+                    });
+                });
             });
-        });
+        }
+        catch (err) {
+            console.log("Cannot find the usage module. Skipping...");
+            request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
+                if (response && response.statusCode == 200) {
+                    if (!body.includes("Connection failed")) {
+                        svStatus = "Operational";
+                    } else {
+                        svStatus = "Cannot connect to the SQL server";
+                    }
+                } else {
+                    svStatus = "Cannot connect to the PHP server!";
+                }
+                var dated = new Date();
+                const mess = {
+                    color: Math.floor(Math.random() * 16777214) + 1,
+                    title: 'BOT\'s current status',
+                    description: "⏰ **BOT's restart time**: " + client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes() + "\n⬆️ **Active time:** " + days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs\n🙂 **Server response:** " + (dated.getTime() - message.createdTimestamp) + " ms\n🖥️ **Server status:** " + svStatus + "\n" + quoteInfo + "🚩 **Joined servers:** " + client.guilds.cache.size + "\n👪 **Number of users:** " + client.users.cache.size + "\n#️⃣ **Channels:** " + client.channels.cache.size + "\n💬 **Number of messages sent (from BOT's last restart):** " + client.sentMessages + "\n🤖 **Number of commands executed (from BOT's last restart):** " + client.botMessages,
+                    footer: {
+                        text: client.devUsername
+                    },
+                };
+                msg.edit({
+                    embed: mess
+                });
+            });
+        }
     });
 }
 
