fda4ad71cdeca3fd52ca5951dd8384e0effa5f88
LilShieru <nambmlc132@gmail.com>
2021-06-18 22:15:13 +0700

    Fixed a serious bug in the Guild Web API

--- web/Guild.js
+++ web/Guild.js
@@ -2,7 +2,6 @@ const request = require("request");
 
 module.exports.get = function(client, req, res) {
     try {
-        res.header("Content-Type", "application/json");
         if (!req.query.id) return res.status(400).send(JSON.stringify({code: 400, error: "No \"id\" query was found."}));
         var guild = client.guilds.cache.get(req.query.id);
         if (!guild) return res.status(404).send(JSON.stringify({code: 404, error: "Cannot find the Guild object. Maybe you entered an invalid Guild ID or the BOT hasn't joined this guild yet."}));
@@ -18,8 +17,8 @@ module.exports.get = function(client, req, res) {
                 avatar_url: guild.owner.user.avatarURL({size: 2048, format: "png", dynamic: true}),
             },
             permissions: guild.permissions,
-            roles: client.economyManager[req.query.id].roles || [],
-            prefix: client.customPrefixes[req.query.id] || "u!"
+            roles: (client.economyManager[req.query.id]) ? (client.economyManager[req.query.id].roles || []) : [],
+            prefix: (client.customPrefixes) ? (client.customPrefixes[req.query.id] || client.config.prefix) : client.config.prefix
         }));
     }
     catch (err) {
