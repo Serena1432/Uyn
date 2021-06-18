79856bf4dd3be2e0a9edc4fd14628eadf28880a9
LilShieru <nambmlc132@gmail.com>
2021-06-18 22:07:02 +0700

    Added the Guild Web API for the website

--- web/GetStatus.js
+++ web/GetStatus.js
@@ -1,51 +1,57 @@
 const request = require("request");
-const reqTime = new Date();
 
 module.exports.get = function(client, req, res) {
-	var reqTime = new Date();
-    let totalSeconds = (client.uptime / 1000);
-    let days = Math.floor(totalSeconds / 86400);
-    let hours = Math.floor(totalSeconds / 3600);
-    totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = totalSeconds % 60;
-    var svStatus = "Unknown";
-    var quoteInfo = "";
-    const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
-    const memoryData = process.memoryUsage();
-    const memoryUsage = {
-        rss: `${formatMemoryUsage(memoryData.rss)}`,
-        heapTotal: `${formatMemoryUsage(memoryData.heapTotal)}`,
-        heapUsed: `${formatMemoryUsage(memoryData.heapUsed)}`,
-        external: `${formatMemoryUsage(memoryData.external)}`,
-    };
-    var os = require('os-utils');
-    os.cpuUsage(cpu => {
-        request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
-            if (response && response.statusCode == 200) {
-                if (!body.includes("Connection failed")) {
-                    svStatus = "Operational";
+    try {
+        res.header("Content-Type", "application/json");
+        var reqTime = new Date();
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = totalSeconds % 60;
+        var svStatus = "Unknown";
+        var quoteInfo = "";
+        const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
+        const memoryData = process.memoryUsage();
+        const memoryUsage = {
+            rss: `${formatMemoryUsage(memoryData.rss)}`,
+            heapTotal: `${formatMemoryUsage(memoryData.heapTotal)}`,
+            heapUsed: `${formatMemoryUsage(memoryData.heapUsed)}`,
+            external: `${formatMemoryUsage(memoryData.external)}`,
+        };
+        var os = require('os-utils');
+        os.cpuUsage(cpu => {
+            request(process.env.php_server_url + '/GetAllQuotes.php', function(error, response, body) {
+                if (response && response.statusCode == 200) {
+                    if (!body.includes("Connection failed")) {
+                        svStatus = "Operational";
+                    } else {
+                        svStatus = "Cannot connect to the SQL server";
+                    }
                 } else {
-                    svStatus = "Cannot connect to the SQL server";
+                    svStatus = "Cannot connect to the PHP server!";
                 }
-            } else {
-                svStatus = "Cannot connect to the PHP server!";
-            }
-            var dated = new Date();
-            var data = {
-                restart_time: client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes(),
-                active_time: days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs",
-                server_ping: (dated.getTime() - reqTime.getTime()) + " ms",
-                server_status: svStatus,
-                joined_servers: client.guilds.cache.size,
-                users: client.users.cache.size,
-                channels: client.channels.cache.size,
-                total_messages: client.sentMessages,
-                bot_messages: client.botMessages,
-                memory_usage: memoryUsage.rss,
-                cpu_usage: cpu.toFixed(2) + "%"
-            };
-            res.send(JSON.stringify(data));
+                var dated = new Date();
+                var data = {
+                    restart_time: client.startTime.getDate() + "/" + (client.startTime.getMonth() + 1) + "/" + client.startTime.getFullYear() + "; " + client.startTime.getHours() + ":" + client.startTime.getMinutes(),
+                    active_time: days + "days, " + hours + "hours, " + minutes + "mins, " + parseInt(seconds) + "secs",
+                    server_ping: (dated.getTime() - reqTime.getTime()) + " ms",
+                    server_status: svStatus,
+                    joined_servers: client.guilds.cache.size,
+                    users: client.users.cache.size,
+                    channels: client.channels.cache.size,
+                    total_messages: client.sentMessages,
+                    bot_messages: client.botMessages,
+                    memory_usage: memoryUsage.rss,
+                    cpu_usage: cpu.toFixed(2) + "%"
+                };
+                res.send(JSON.stringify(data));
+            });
         });
-    });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send(JSON.stringify({code: 500, error: err.toString()}));
+    }
 }
---
+++ web/Guild.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+module.exports.get = function(client, req, res) {
+    try {
+        res.header("Content-Type", "application/json");
+        if (!req.query.id) return res.status(400).send(JSON.stringify({code: 400, error: "No \"id\" query was found."}));
+        var guild = client.guilds.cache.get(req.query.id);
+        if (!guild) return res.status(404).send(JSON.stringify({code: 404, error: "Cannot find the Guild object. Maybe you entered an invalid Guild ID or the BOT hasn't joined this guild yet."}));
+        res.send(JSON.stringify({
+            id: guild.id,
+            name: guild.name,
+            icon_url: guild.iconURL({size: 2048, format: "png", dynamic: true}),
+            owner: {
+                id: guild.owner.user.id,
+                name: guild.owner.user.username,
+                discriminator: guild.owner.user.discriminator,
+                tag: guild.owner.user.tag,
+                avatar_url: guild.owner.user.avatarURL({size: 2048, format: "png", dynamic: true}),
+            },
+            permissions: guild.permissions,
+            roles: client.economyManager[req.query.id].roles || [],
+            prefix: client.customPrefixes[req.query.id] || "u!"
+        }));
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send(JSON.stringify({code: 500, error: err.toString()}));
+    }
+}
