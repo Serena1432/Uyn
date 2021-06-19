9066a46e8a8e0ca805e8cc65002be8d77d4dfc91
LilShieru <nambmlc132@gmail.com>
2021-06-19 10:39:55 +0700

    Fixed a bug in the NoMemberGuild Web API

--- web/NoMemberGuild.js
+++ web/NoMemberGuild.js
@@ -5,8 +5,6 @@ module.exports.get = async function(client, req, res) {
         if (!req.query.id) return res.status(400).send(JSON.stringify({code: 400, error: "No \"id\" query was found."}));
         var guild = client.guilds.cache.get(req.query.id);
         if (!guild) return res.status(404).send(JSON.stringify({code: 404, error: "Cannot get the Guild object. Maybe you entered an invalid Guild ID or the BOT hasn't joined this guild yet."}));
-        var member = guild.member(id);
-        if (!member) return res.status(404).send(JSON.stringify({code: 404, error: "Cannot get the member information. Are you trying to view information of a guild that you haven't joined?"}));
         res.send(JSON.stringify({
             id: guild.id,
             name: guild.name,
